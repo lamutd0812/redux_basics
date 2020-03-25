035311fd590ad8bdfc9c7e3f4e6086cf90622867
lamutd0812 <lamutd0812@outlook.com>
2020-03-25 22:25:59 +0700

    15-redux-advanced: end

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,28 +1,27 @@
-import * as actionTypes from '../actions';
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElementId);
+    // filter method not change the old array
+    return updateObject(state, { results: updatedArray });
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
+            return updateObject(state, {
                 results: state.results.concat(
-                    { id: new Date(), value: action.result }
+                    { id: Math.floor(Math.random() * 10000), value: action.result }
                 )
-            }
+            });
+
         case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const updatedArray = [...state.results];
-            // updatedArray.splice(id, 1); // another way
-            const updatedArray = state.results.filter(result => result.id !== action.resultElementId);
-            // filter method not change the old array
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return deleteResult(state, action);
         default:
             return state;
     }
